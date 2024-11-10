72da4dbdc5791668ff81637ad971f90f52067712
migue <mmoyano16@gmail.com>
2024-11-10 20:54:51 -0300

    feat: add create reace handler

--- src/delivery/controllers/RaceController.ts
+++ src/delivery/controllers/RaceController.ts
@@ -5,13 +5,23 @@ import Authorization from "../middlewares/Authorization";
 const RaceController = (actions: Actions) => {
 
   const getRacesHandler = async (ctx: Context) => {
+    console.log(">> entra al handler de get")
     const races = await actions.getRacesAction.execute();
+    console.log(">> las races", races)
     return races;
+  }
+
+  const createRaceHandler = async (ctx: Context) => {
+    console.log(">> entra al handler de create")
+    const race = await actions.createRaceAction.execute();
+    console.log(">> la race", race)
+    return race.getData();
   }
 
   return new Elysia()
     .use(Authorization)
     .get("/races", getRacesHandler)
+    .post("/races", createRaceHandler)
 
 }
 
